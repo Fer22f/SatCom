e9ef23e62355da7d440065cc921515ea366150f9
Fer22f <fer22f@gmail.com>
2014-01-14 18:34:21 -0200

    Did some work on the texture and made the GUI work

--- src/gui/ContainerSatellite.java
+++ src/gui/ContainerSatellite.java
@@ -19,10 +19,20 @@ public class ContainerSatellite extends Container {
 
         for (i = 0; i < 3; ++i)
         {
-            for (j = 0; j < 3; ++j)
+           this.addSlotToContainer(new Slot(tile, i, 62, 17 + i * 18));
+        }
+
+        for (i = 0; i < 3; ++i)
+        {
+            for (j = 0; j < 9; ++j)
             {
-                this.addSlotToContainer(new Slot(tile, j + i * 3, 62 + j * 18, 17 + i * 18));
+                this.addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
             }
+        }
+
+        for (i = 0; i < 9; ++i)
+        {
+            this.addSlotToContainer(new Slot(inventory, i, 8 + i * 18, 142));
         }
 	}
 
