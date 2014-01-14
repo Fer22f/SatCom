a9a5725d7901d21a05623e2b0262bf22ce2e22bc
Fer22f <fer22f@gmail.com>
2014-01-14 17:48:23 -0200

    New TileEntity

--- src/block/BlockSatellite.java
+++ src/block/BlockSatellite.java
@@ -4,17 +4,23 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Icon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import fer22f.mods.satcom.SatCom;
+import fer22f.mods.satcom.tile.TileSatellite;
 
 public class BlockSatellite extends BlockContainer {
 
+	@SideOnly(Side.CLIENT)
 	private static Icon topIcon;
-	private static Icon sideIcon;
+	@SideOnly(Side.CLIENT)
+	private static Icon faceIcon;
 	
 	public BlockSatellite() {
 		super(SatCom.satelliteID, Material.iron);
@@ -28,14 +34,26 @@ public class BlockSatellite extends BlockContainer {
 	@SideOnly(Side.CLIENT)
     public void registerIcons(IconRegister par1IconRegister)
     {
-        this.blockIcon = par1IconRegister.registerIcon("satcom:satellite");
+        this.blockIcon = par1IconRegister.registerIcon("satcom:satellite_side");
         this.topIcon = par1IconRegister.registerIcon("satcom:satellite_top");
-        this.sideIcon = par1IconRegister.registerIcon("satcom:satellite_side");
+        this.faceIcon = par1IconRegister.registerIcon("satcom:satellite");
+    }
+	
+	@SideOnly(Side.CLIENT)
+    public Icon getIcon(int par1, int par2)
+    {
+        return par1 == 1 ? this.topIcon : (par1 == 0 ? this.topIcon : (par2 == 2 && par1 == 2 ? this.faceIcon : (par2 == 3 && par1 == 5 ? this.faceIcon : (par2 == 0 && par1 == 3 ? this.faceIcon : (par2 == 1 && par1 == 4 ? this.faceIcon : this.blockIcon)))));
+    }
+	
+	public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack)
+    {
+        int l = MathHelper.floor_double((double)(par5EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 2.5D) & 3;
+        par1World.setBlockMetadataWithNotify(par2, par3, par4, l, 2);
     }
 
 	@Override
 	public TileEntity createNewTileEntity(World world) {
-		return null;
+		return new TileSatellite();
 	}
 
 }
