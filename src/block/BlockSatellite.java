505cfd290a7d43ff8bb6929fecd4c64ee9b1df10
Fer22f <fer22f@gmail.com>
2014-01-14 17:29:36 -0200

    New textures and some changes

--- src/block/BlockSatellite.java
+++ src/block/BlockSatellite.java
@@ -5,6 +5,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -12,6 +13,9 @@ import fer22f.mods.satcom.SatCom;
 
 public class BlockSatellite extends BlockContainer {
 
+	private static Icon topIcon;
+	private static Icon sideIcon;
+	
 	public BlockSatellite() {
 		super(SatCom.satelliteID, Material.iron);
 		this.setHardness(10.0F);
@@ -25,6 +29,8 @@ public class BlockSatellite extends BlockContainer {
     public void registerIcons(IconRegister par1IconRegister)
     {
         this.blockIcon = par1IconRegister.registerIcon("satcom:satellite");
+        this.topIcon = par1IconRegister.registerIcon("satcom:satellite_top");
+        this.sideIcon = par1IconRegister.registerIcon("satcom:satellite_side");
     }
 
 	@Override
