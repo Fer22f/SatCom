8ecf1e709868d157c36d02b45884a8c482fbcebc
Fer22f <fer22f@gmail.com>
2014-01-14 16:53:57 -0200

    Made the first variables/functions
    Now I started to make the core

--- src/SatCom.java
+++ src/SatCom.java
@@ -1,5 +1,20 @@
 package fer22f.mods.satcom;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid="SatCom", name="SatCom", version="0.0.0")
+@NetworkMod(clientSideRequired=true)
 public class SatCom {
 
+	@Instance(value = "SatCom")
+    public static SatCom instance;
+	
+	@EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+            
+    }
 }
